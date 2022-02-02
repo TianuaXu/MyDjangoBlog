fb8f216e917389a892e7e81d34b5dc4dcca8acf3
TianuaXu <2756432588@qq.com>
2022-02-02 16:27:39 +0800

    "debug"

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'eeuyb!8_3tr=@utk(mll8m$z_30=5g!ars$9@5s9u%^m9atafv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['39.105.124.244', 'localhost']
 
